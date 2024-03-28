5efddd9928fc0f20c2f23c4577fc10c1a59be671
Cao Hào Lê <lecaohao2101@gmail.com>
2024-03-28 20:46:55 +0700

    config database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const cors = require("cors");
+const path = require("path");
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(cors());
+
+//config database
+const dbConfig = {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "lms",
+};
+const pool = mysql.createPool(dbConfig);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render("home", {title: "WEB - 1640"})
 });
 app.listen(PORT, () => {
     console.log(`Server run on ${PORT}`);
