9fe43634adbec37750903b91b88a0e087273b4e2
Phuoc2610 <phuocnvgcd191236@fpt.edu.vn>
2024-03-31 18:57:39 +0700

    manage post with coordinator

--- src/router/coordinator/index.js
+++ src/router/coordinator/index.js
@@ -13,9 +13,7 @@ const dbConfig = {
 };
 const pool = mysql.createPool(dbConfig);
 
-
 router.use(checkLoggedIn)
-
 
 router.get("/coordinator_page", async (req, res) => {
    const connection = await pool.getConnection();
@@ -42,5 +40,23 @@ router.get("/coordinator_manage_student", async (req, res) => {
        students: rows,
    });
 });
+
+//manage post
+router.get("/coordinator_manage_post", async (req, res) => {
+    const connection = await pool.getConnection();
+    const manager_id = req.cookies.uid;
+    const [rows0] = await connection.query(
+        "SELECT * FROM departmentManager INNER JOIN faculty ON faculty.department_id = departmentManager.department_id WHERE departmentManager.department_manager_id= ?",
+        [manager_id]
+    );
+    const [rows] = await connection.query(
+        "SELECT * FROM post INNER JOIN student ON post.article_author_id = student.student_id WHERE student.student_department_id= ?",
+        [rows0[0].department_id]
+    );
+    connection.release();
+    console.log(rows);
+    res.render("coordinator/coordinator_manage_post", { title: "Post manager", posts: rows });
+ });
+ 
 module.exports = router;
 
