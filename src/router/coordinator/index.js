6a8821fb31d017ba2af4a7859859329554b4454c
vanphuoc <phuoc0905886611@gmail.com>
2024-03-31 11:08:29 +0700

    manage student with coordinator

--- src/router/coordinator/index.js
+++ src/router/coordinator/index.js
@@ -1,24 +1,46 @@
+
 const express = require("express");
 const router = express.Router();
 const mysql = require("mysql2/promise");
 const checkLoggedIn = require("../../middleware/checkLogin");
-const checkAdminRole = require("../../middleware/checkAdmin");
 
 //config database
 const dbConfig = {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "lms",
+   host: "localhost",
+   user: "root",
+   password: "",
+   database: "lms",
 };
 const pool = mysql.createPool(dbConfig);
 
+
 router.use(checkLoggedIn)
 
-router.get("/coordinator_page", checkAdminRole, async (req, res) => {
-    const connection = await pool.getConnection();
-    connection.release();
-    res.render("coordinator/coordinator_page", {title: "Coordinator"});
+
+router.get("/coordinator_page", async (req, res) => {
+   const connection = await pool.getConnection();
+   connection.release();
+   res.render("coordinator/coordinator_page", {title: "Coordinator"});
 });
 
+//manage student
+router.get("/coordinator_manage_student", async (req, res) => {
+   const connection = await pool.getConnection();
+   const manager_id = req.cookies.uid;
+   const [rows0] = await connection.query(
+       "SELECT * FROM departmentManager INNER JOIN faculty ON faculty.department_id = departmentManager.department_id WHERE departmentManager.department_manager_id= ?",
+       [manager_id]
+   );
+   const department_id = rows0[0].department_id;
+   const [rows] = await connection.query(
+       "SELECT * FROM student WHERE student_department_id = ?",
+       [department_id]
+   );
+   connection.release();
+   res.render("coordinator/coordinator_manage_student", {
+       title: "Student manager",
+       students: rows,
+   });
+});
 module.exports = router;
+
